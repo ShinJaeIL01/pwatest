d08993c21015a2c6732e1792fda61ccbacd868ea
ShinJaeIL01 <jay101@nextlab.co.kr>
2024-08-07 11:15:08 +0900

    install

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isShown, setIsShown] = useState(false);
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+      setIsShown(true);
+    };
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
 
+    return () => {
+      window.removeEventListener(
+        "beforeinstallprompt",
+        handleBeforeInstallPrompt
+      );
+    };
+  }, []);
   return (
     <>
       <div>
@@ -21,6 +37,20 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        {isShown && (
+          <button
+            onClick={async () => {
+              setIsShown(false);
+              if (!deferredPrompt) {
+                return;
+              }
+              deferredPrompt.prompt();
+              setDeferredPrompt(null);
+            }}
+          >
+            install
+          </button>
+        )}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +59,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
